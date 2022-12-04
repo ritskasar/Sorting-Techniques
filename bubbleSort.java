f046c41692f746b94b6da1b6c159606d17528c0e
Rutika Dilip Kasar <65780326+ritskasar@users.noreply.github.com>
2022-12-04 15:18:51 +0530

    Bubble Sort

---
+++ bubbleSort.java
@@ -0,0 +1,47 @@
+import java.util.*;
+class bubbleSort
+{
+
+	public static void SortNo(int arr[])           // function declaration....
+	{
+		System.out.println("Sorted Elements are...");
+
+		for(int i=0;i<arr.length;i++)       // time complexity of
+		{
+			System.out.print(arr[i] +" ");     // output of arrays....
+		}
+
+		System.out.println();
+	}
+
+	public static void main(String[]args)
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter size of array");
+		int size = in.nextInt();
+		System.out.println("Enter numbers into array...");
+		int arr[]=new int [size];                      // declaration of array
+
+		for(int i=0;i<arr.length;i++)
+		{                                     // input of array
+			arr[i]=in.nextInt();
+		}
+
+
+		for(int i=0;i<arr.length-1;i++)          //outer loop
+		{
+			for(int j=0;j<arr.length-1-i;j++)     //inner loop
+			{
+				if(arr[j]>arr[j+1])
+				{
+					int temp = arr[j+1];         //swapping elements
+					arr[j+1]=arr[j];
+					arr[j]=temp;
+				}
+			}
+		}
+
+		SortNo(arr);                        //function calling
+
+	}
+}
